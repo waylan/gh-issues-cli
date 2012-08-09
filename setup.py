11a074314289bc4a620ad57c55f3177649a1064b
Waylan Limberg <waylan@gmail.com>
2012-08-09 05:16:00 -0400

    Added setup.py script.

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name =          'gh-issues-cli',
+    version =       '0.1',
+    url =           'https://github.com/waylan/gh-issues-cli/',
+    download_url =  '',
+    description =   'GitHub Issues Commandline Interface.',
+    author =        'Waylan limberg',
+    author_email =  'waylan [at] gmail.com',
+    license =       'BSD License',
+    scripts =       ['git-issues'],
+)
